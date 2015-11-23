2541b95df18a76c90ca6dc765a117a72a18cb29d
Andriy Suden <andriykosud@gmail.com>
2015-11-23 17:54:33 -0600

    Overloaded + and - operators to add and subtract anums, still need to finish eat function

--- anum.cpp
+++ anum.cpp
@@ -4,7 +4,7 @@
 std::ostream& operator << (std::ostream &output, Anum const& someNum){
     
     // todo: delete this section later, only used for debugging now
-    output << "Your num is: ";
+    output << "Your num is:   ";
     
     // print negative sign if negative number
     if (someNum.isNegative) output.put('-');
@@ -21,9 +21,11 @@ std::istream& operator >> (std::istream &input, Anum& someNum){
     
     // declare variables
     char buffer;
-    std::vector <unsigned> numvec;
+    std::vector <int> numvec;
     
     // output instructions and make sure std::ostream is clear by flushing
+    //std::cout.flush();
+    
     // todo: delete this section later, only used for debugging for now
     std::cout << "Enter integer: ";
     std::cout.flush();
@@ -35,16 +37,16 @@ std::istream& operator >> (std::istream &input, Anum& someNum){
     input >> std::ws;
     
     // check if user wants positive or negative number
-    // if negative, eat the first char
+    // if positive or negative, eat the first char
     if (input.peek() == '-') someNum.isNegative = 1;
     if (input.peek() == '+' || input.peek() == '-') input.get();
     
     // infinite loop, adding infinite amount of numbers to numvec
     while (1){
         buffer = input.peek();
-        unsigned i;
+        int i;
         
-        if (buffer >= '0' && buffer <= '9') i = (unsigned)(buffer - '0');
+        if (buffer >= '0' && buffer <= '9') i = (int)(buffer - '0');
         else if (buffer == ' ' || buffer == '\n' || buffer == '\t') break;
         else{
             input.setstate(std::ios_base::failbit);
@@ -67,10 +69,73 @@ std::istream& operator >> (std::istream &input, Anum& someNum){
     return input;
 }
 
+
+Anum operator + (Anum num1, Anum num2){
+    // We want the bigger one (in absolute value) to be in num1
+    if (!num1.is_greater_than(num2)) std::swap(num1, num2);
+    
+    // since num1 now stores the largest element,
+    // the return value of num1 will have the correct
+    // sign (- or +)
+    
+    // The size of the new vector will never exceed 1+the size of the largest one
+    unsigned sizevec = num1.num.size()+1;
+    
+    // We append 0's to the fronts of both numbers, to make the sizes equal to sizevec
+    num1.num.push_back(0);
+    for (unsigned i = num2.num.size(); i < sizevec; i++) {
+        num2.num.push_back(0);
+    }
+    
+    // if both are negative, just add every element to the corresponding element
+    if (num1.isNegative == num2.isNegative) {
+        for (int i = 0; i<=sizevec-1; i++){
+            
+            num1.num[i]+=num2.num[i];
+            
+            // If the number at the index is greater than 10, we need to carry
+            if (num1.num[i] >= 10) {
+                int wholes = num1.num[i]/10;
+                num1.num[i+1]+= wholes;
+                num1.num[i]-=(10*wholes);
+            }
+        }
+        //num1.eat();
+        return num1;
+    }
+    // if the signs of both are different, with the first one being larger
+    else{
+        for (int i = 0; i<=sizevec-1; i++){
+            
+            num1.num[i]-=num2.num[i];
+            
+            // if the number at the index is less than 10, we need to borrow
+            if (num1.num[i] < 0) {
+                int wholes = num1.num[i]/10+1;
+                num1.num[i+1]-= wholes;;
+                num1.num[i]+=wholes*10;
+            }
+        }
+        //num1.eat();
+        return num1;
+    }
+
+    return num1;
+}
+
+Anum operator - (Anum num1, Anum num2){
+    // simply return the sum of num1 and negative num2, since + is already overloaded to subtract
+    return num1 + -num2;
+}
+
 int main(int argc, char *argv[]){
     
-    Anum someNumber;
-    std::cin >> someNumber;
-    std::cout << someNumber << std::endl;
+    std::cout.sync_with_stdio(false);
+    
+    Anum someguy;
+    std::cin >> someguy;
+    std::cout << someguy << std::endl;
+    std::cout << someguy + someguy << std::endl;
+    
     return 0;
 }
--- anum.h
+++ anum.h
@@ -9,17 +9,52 @@
 
 class Anum {
     
-    std::vector <unsigned> num;
-    unsigned number;
+    std::vector <int> num;
     bool isNegative;
     
-public:
+    bool is_greater_than(Anum num2){
+        
+        if (num.size() != num2.num.size()){
+            if (num.size() > num2.num.size()) return 1;
+            else return 0;
+        }
+        
+        else{
+            for (int i = num.size()-1; i>=0; i--) {
+                if (num[i]>num2.num[i]){
+                    return 1;
+                }
+            }
+        }
+        
+        return 0;
+    }
     
-    Anum(unsigned num): number(num) {  };
+    Anum eat(){
+        for (int i = num.size()-1; i >= 0; i--) {
+            if (num[i] != 0) {
+                return *this;
+            }
+            else{
+                num.pop_back();
+            }
+        }
+        return *this;
+    }
+    
+public:
     Anum() { num.push_back(0); };
     
     friend std::ostream& operator << (std::ostream &output, Anum const &someNum);
     friend std::istream& operator >> (std::istream &input,  Anum &someNum);
+    friend Anum operator + (Anum num1, Anum num2);
+    friend Anum operator - (Anum num1, Anum num2);
+    
+    Anum operator -() {
+        Anum number = *this;
+        number.isNegative = !number.isNegative;
+        return number;
+    }
     
 };
 
